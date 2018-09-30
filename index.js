50028342c8b6818fe92386f270014827f90b0565
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 19:28:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n){
+  var lis = document.queryElementById("app").querySelectorAll(".ranked-list");
+  
+  for(var i = 0; i < lis.length; i++){
+    lis[i].innerHTML = toString(parseInt(lis[i].innerHTML) + n);
+  }
+  
+}
+
+function deepestChild(){
+  var divs = document.queryElementById("grand-node").querySelectorAll("div");
+  
+  
+}
